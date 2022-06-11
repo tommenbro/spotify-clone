560d2b3353294d8f52ae803f9d6c0292e218866b
tommenbro <74247764+tommenbro@users.noreply.github.com>
2022-06-11 02:50:22 +0200

    vercelllyyyy

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@ module.exports = {
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
